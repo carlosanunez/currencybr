8d0c44c8e0a099930c833db93557a107b84fcdeb
André Ribas <ribas.andre@gmail.com>
2013-02-21 08:14:12 -0300

    corrigido o nome do arquivo e recurso de classe

---
+++ libraries/currencybr.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Bundle conversor de valor monetario no formato FLOAT para o padrão brasileiro em REAIS
+ * 
+ * @package     currencybr
+ * @author      Andre Ribas <zitoloco@gmail.com>
+ * @basedon     
+ */
+
+class CurrencyBR{
+
+	public static function toMysql($value)
+    {
+		$float = preg_replace('/[^,0-9]/i', '', $value);
+        return str_replace(',', '.', $float);
+	}
+	
+	public static function toView($value, $color = false)
+    {	
+        $retorno = "sem valor";
+        if ($color && $value != 0)
+        {
+            if ($value < 0)
+            {
+                $class = 'negativo';
+            }
+            else
+            {
+                $class = 'positivo';
+            }
+		    $retorno = '<span class="' . $class . '">R$ ' . number_format($value, 2, ',', '.') . '</span>';
+        } 
+        else 
+        {
+		    $retorno = 'R$ ' . number_format($value, 2, ',', '.');
+        }
+        return $retorno;
+	}
+}
